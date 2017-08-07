aa3d58a81fb3dc767dc8a6fc224b3ec70c7d42b8
Zhdanov Vladimir <zhdanjj@gmail.com>
2017-08-07 17:04:36 +0300

    Refactoring

--- js/bg.js
+++ js/bg.js
@@ -1,7 +1,22 @@
+var API_KEY = 'dict.1.1.20141016T192728Z.09188df21d2c7744.34e10163a71f9f5fb431051d89c25c6cd609af60';
+
+function getWordTranslation(word, callback) {
+  var url = 'https://dictionary.yandex.net/api/' +
+            'v1/dicservice.json/lookup?' +
+            'key=' + API_KEY +
+            '&lang=en-ru&ui=ru&text=' + word;
+  fetch(url)
+   .then((response) => {
+     return response.json()
+   })
+   .then((json) => {
+     callback(json)
+   })
+}
+
 chrome.runtime.onMessage.addListener(function(message, sender, callback){
-    chrome.tabs.query({}, function(tabs) {
-        for (var i=0; i<tabs.length; i++) {
-            chrome.pageAction.show(tabs[i].id);
-        }
-    });
+  if (message && message.word) {
+    getWordTranslation(message.word, callback)
+  }
+  return true
 });
--- js/content.js
+++ js/content.js
@@ -1,151 +1,243 @@
-function getWordAtPoint(elem, x, y) {
-    var range;
-    if (elem.nodeType == elem.TEXT_NODE) {
-        range          = elem.ownerDocument.createRange();
-        range.selectNodeContents(elem);
-        var currentPos = 0;
-        var endPos     = range.endOffset;
-        while (currentPos + 1 < endPos) {
-            range.setStart(elem, currentPos);
-            range.setEnd(elem, currentPos + 1);
-            if (range.getBoundingClientRect().left <= x && range.getBoundingClientRect().right >= x &&
-                range.getBoundingClientRect().top <= y && range.getBoundingClientRect().bottom >= y) {
-                range.expand("word");
-                var ret = range.toString();
-                range.detach();
-                return (ret);
-            }
-            currentPos += 1;
-        }
-    }
-    else {
-        for (var i = 0; i < elem.childNodes.length; i++) {
-            range = elem.childNodes[i].ownerDocument.createRange();
-            range.selectNodeContents(elem.childNodes[i]);
-            if (range.getBoundingClientRect().left <= x && range.getBoundingClientRect().right >= x &&
-                range.getBoundingClientRect().top <= y && range.getBoundingClientRect().bottom >= y) {
-                range.detach();
-                return (getWordAtPoint(elem.childNodes[i], x, y));
-            }
-            else {
-                range.detach();
-            }
-        }
-    }
-    return (null);
+function l(arg) {
+  console.log.apply(console, arguments)
 }
-var API_KEY = 'dict.1.1.20141016T192728Z.09188df21d2c7744.34e10163a71f9f5fb431051d89c25c6cd609af60';
-var content =
-        '<div id="hodi-word"></div>' +
-        '<hr  id="hodi-line">' +
-        '<div id="hodi-preloader"></div>' +
-        '<div  id="hodi-translations"></div>';
-var popup   = {
-    init       : function () {
-        this.box             = document.createElement('div');
-        this.box.style.display   = 'none';
-        this.box.id          = 'hodi-box';
-        this.box.innerHTML   = content;
-        document.body.appendChild(this.box);
-        this.word            = document.getElementById('hodi-word');
-        this.preloader       = document.getElementById('hodi-preloader');
-        this.preloader
-            .style
-            .backgroundImage =
-            'url("' + chrome.extension.getURL('img/preloader.gif') + '")';
-        this.translations    = document.getElementById('hodi-translations');
 
-        document.addEventListener('mousemove', function (e) {
-            x       = e.clientX;
-            y       = e.clientY;
-            var pos = popup.getPosition();
-            //скрываем окошко когда мышь уходит
-            if (!popup.isHidden()) {
-                if (Math.abs(x + 10 - pos.x) > 20
-                    || Math.abs(y + 10 - (pos.y - window.pageYOffset)) > 20) {
-                    popup.hide();
-                }
-            }
-        });
+var cursor = {
+  x: 0,
+  y: 0,
+  init: function () {
+    document.addEventListener('mousemove', this.onMouseMove.bind(this))
+  },
+  onMouseMove: function (event) {
+    this.x = event.clientX
+    this.y = event.clientY
+  }
+}
 
-        document.addEventListener('keyup', function (e) {
-            if (e.which === 16) { //Shift key
-                var word = getWordAtPoint(document.elementFromPoint(x, y), x, y);
-                if (word !== null) {
-                    popup.show(x, y, word);
-                }
-            }
-        });
-    },
-    isHidden   : function () {
-        return this.box.style.display === 'none';
-    },
-    hide       : function () {
-        this.box.style.display = 'none';
-    },
-    show       : function (x, y, word) {
-        this.box.style.left             = x + 10 + 'px';
-        this.box.style.top              = y + 10 + window.pageYOffset + 'px';
-        this.box.style.display          = 'block';
-        this.word.innerText             = word;
-        this.preloader.style.display    = 'block';
-        this.translations.style.display = 'none';
-        this.translations.innerHTML     = '';
+var wordHighlight = {
+  init: function () {
+    this.element = document.createElement('div')
+    this.element.className = 'hodi-highlight'
+    document.body.appendChild(this.element)
+    document.addEventListener('scroll', this.onWindowScroll.bind(this))
+    document.addEventListener('click', this.onDocumentClick.bind(this))
+  },
+  show: function () {
+    this.element.style.display = 'block'
+    return this
+  },
+  hide: function () {
+    this.element.style.display = 'none'
+    return this
+  },
+  setRect: function (rect) {
+    this.element.style.top = rect.top + 'px'
+    this.element.style.left = rect.left + 'px'
+    this.element.style.width = rect.width + 'px'
+    this.element.style.height = rect.height + 'px'
+    return this
+  },
+  onWindowScroll: function () {
+    this.hide()
+  },
+  onDocumentClick: function () {
+    this.hide()
+  }
+}
 
-        var request                = new XMLHttpRequest();
-        var url                    = 'https://dictionary.yandex.net/api/' +
-                                     'v1/dicservice.json/lookup?' +
-                                     'key=' + API_KEY +
-                                     '&lang=en-ru&ui=ru&text=' + word;
-        request.open('get', url, true);
-        request.onreadystatechange = function () {
-            //структура описана здесь
-            //https://tech.yandex.ru/dictionary/doc/dg/reference/lookup-docpage/
-            if (request.readyState !== 4) {
-                return;
-            }
-            var objAnswer = JSON.parse(request.responseText);
-            var defs      = objAnswer['def'];
-            for (var i = 0; i < defs.length; i++) {
-                var def = document.createElement('div');
-                var trs = [];
-                for (var j = 0; j < defs[i]['tr'].length; j++) {
-                    trs.push(defs[i]['tr'][j]['text']);
-                }
-                def.innerText = trs.join(', ');
+var popup = {
+  html: '<div id="hodi-word"></div><hr  id="hodi-line"><div id="hodi-preloader"></div><div id="hodi-translations"></div>',
+  init: function () {
+    this.box = document.createElement('div');
+    this.box.id = 'hodi-box';
+    this.box.innerHTML = this.html;
+    document.body.appendChild(this.box);
 
-                var pos       = document.createElement('span');
-                pos.className = 'hodi-pos';
-                pos.innerText = ' ' + defs[i]['pos'];
-                def.appendChild(pos);
-                popup.translations.appendChild(def);
-            }
-            if (!defs.length) {
-                popup.translations.innerText = 'По этому слову ничего нет.';
-            }
-            popup.preloader.style.display    = 'none';
-            popup.translations.style.display = 'block';
-        };
-        request.send(null);
-    },
-    /**
-     *
-     * @returns {{x: number, y: number}}
-     */
-    getPosition: function () {
-        return {
-            x: Number(this.box.style.left.replace('px', '')),
-            y: Number(this.box.style.top.replace('px', ''))
-        }
+    this.word = document.getElementById('hodi-word');
+
+    this.preloader = document.getElementById('hodi-preloader');
+    this.preloader
+      .style
+      .backgroundImage =
+      'url("' + chrome.extension.getURL('img/preloader.gif') + '")';
+    this.translations = document.getElementById('hodi-translations');
+
+    window.addEventListener('scroll', this.onWindowScroll.bind(this))
+    document.addEventListener('click', this.onDocumentClick.bind(this))
+
+  },
+  hide: function () {
+    this.box.classList.remove('visible')
+    return this
+  },
+  show: function (x, y) {
+    this.box.style.left = x + 10 + 'px';
+    this.box.style.top = y + 10 + window.pageYOffset + 'px';
+    this.box.classList.add('visible')
+    return this
+  },
+  setTitle: function (word) {
+    this.word.textContent = word.toLowerCase()
+    return this
+  },
+  setContent: function (markup) {
+    this.translations.innerHTML = ''
+    this.translations.appendChild(markup)
+    return this
+  },
+  onWindowScroll: function () {
+    this.hide()
+  },
+  onDocumentClick: function () {
+    if (event.target !== this.box || !this.box.contains(event.target)) {
+      this.hide()
     }
-};
+  },
+  enablePreloader: function () {
+    this.preloader.style.display = 'block'
+    this.translations.style.display = 'none'
+    return this
+  },
+  disablePreloader: function () {
+    this.preloader.style.display = 'none'
+    this.translations.style.display = 'block'
+    return this
+  }
+}
 
-window.addEventListener('load', function () {
-    console.log('Window is loaded.');
-    popup.init();
-});
+var selection = {
+  isTextSelected: false,
+  init: function () {
+    document.addEventListener('mouseup', this.onMouseUp.bind(this))
+  },
+  onMouseUp: function () {
+    var text = getSelection().toString()
 
-var x, y;
+    this.isTextSelected = text.length > 0 ? true : false
+  }
+}
 
-chrome.runtime.sendMessage({});
-console.log('HoverDict injected.');
+var api = {
+  getTranslationFor: function (word, callback) {
+    // Здесь будет отсылаться сообщение фоновому скрипту
+
+    chrome.runtime.sendMessage({word: word}, null, callback)
+    // setTimeout(function () {
+    //   callback({defs: []})
+    // }, 500)
+  },
+  createMarkupFrom: function (json) {
+    //структура описана здесь
+    //https://tech.yandex.ru/dictionary/doc/dg/reference/lookup-docpage/
+
+    var markup = document.createElement('div')
+
+    var defs = json['def'];
+    for (var i = 0; i < defs.length; i++) {
+      var def = document.createElement('div');
+      var trs = [];
+      for (var j = 0; j < defs[i]['tr'].length; j++) {
+        trs.push(defs[i]['tr'][j]['text']);
+      }
+      def.innerText = trs.join(', ');
+
+      var pos = document.createElement('span');
+      pos.className = 'hodi-pos';
+      pos.innerText = ' ' + defs[i]['pos'];
+      def.appendChild(pos);
+      markup.appendChild(def);
+    }
+
+    if (!defs.length) {
+      var div = document.createElement('div')
+      div.textContent = 'There\'s no translation for this word'
+      markup.appendChild(div)
+    }
+
+    return markup
+  }
+}
+
+var app = {
+  init: function () {
+    cursor.init()
+    wordHighlight.init()
+    popup.init()
+    selection.init()
+
+    document.addEventListener('keydown', this.onKeyDown.bind(this))
+  },
+  getWordUnderCursor: function () {
+    var el = document.elementFromPoint(cursor.x, cursor.y)
+
+    for (var i = 0; i < el.childNodes.length; i++) {
+      var currentTextNode = el.childNodes[i]
+
+      if (currentTextNode.nodeType !== 3) { // TEXT_NODE
+        continue
+      }
+
+      var range = document.createRange()
+      var word = null
+
+      currentTextNode.textContent.replace(/\b\w+\b/g, function (match, offset) {
+        range.setStart(currentTextNode, offset)
+        range.setEnd(currentTextNode, offset + match.length)
+
+        var r = range.getBoundingClientRect()
+
+        var horizontal = r.left <= cursor.x && r.right >= cursor.x
+        var vertical = r.top <= cursor.y && r.bottom >= cursor.y
+
+        if (horizontal && vertical) {
+          word = {
+            value: range.toString(),
+            rect: r
+          }
+        }
+
+      })
+
+      range.detach()
+
+      if (word) {
+        return word
+      }
+    }
+  },
+  onKeyDown: function (event) {
+    // TODO: тип нажатой кнопки должен браться из настроек
+    if (event.key === 'Shift') {
+      this.handlePopup()
+    }
+  },
+  handlePopup: function () {
+    var word = this.getWordUnderCursor()
+
+    if (!word) {
+      return
+    }
+
+    wordHighlight.setRect(word.rect).show()
+
+    popup
+      .hide()
+      .enablePreloader()
+      .setTitle(word.value)
+      .show(cursor.x, cursor.y)
+
+    this.makeRequest(word.value)
+  },
+  makeRequest: function (word) {
+    api.getTranslationFor(word, function (json) {
+      var markup = api.createMarkupFrom(json)
+      popup
+        .setContent(markup)
+        .disablePreloader()
+    })
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  app.init()
+})
